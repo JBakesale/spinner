031d77e73c08ebd1dd841a69b10dea0d43a72f5a
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-04 17:43:35 -0700

    spinner func revised using typewriter code

---
+++ spinner2.js
@@ -0,0 +1,20 @@
+spinner = (arr) => {
+  let time = 100;
+  for (const i of arr) {
+    setTimeout(() => {
+      process.stdout.write(`\r${i}       `);
+    }, time);
+    time += 200;
+  }
+
+  setTimeout(() => {
+    console.log('\n');
+  }, time + 100);
+}
+
+
+const spinArray = ["|", "/", "-", "\\", "|", "-", "/"];
+
+console.log(spinArray[3]);
+
+spinner(spinArray);
